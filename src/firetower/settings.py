3689e1a01b2d33ec369371f19f5727ef5620b0b1
Spencer Murray <spencer.murray@sentry.io>
2025-09-26 16:18:39 -0400

    remove unnecessary comment

--- src/firetower/settings.py
+++ src/firetower/settings.py
@@ -138,10 +138,6 @@ JIRA = {
     "DOMAIN": os.environ.get("JIRA_DOMAIN", "https://getsentry.atlassian.net"),
     "ACCOUNT": os.environ.get("JIRA_ACCOUNT"),
     "API_KEY": os.environ.get("JIRA_API_KEY"),
-    "PROJECT_KEY": os.environ.get(
-        "JIRA_PROJECT_KEY", "TESTINC"
-    ),  # Default to test project
-    "SEVERITY_FIELD": os.environ.get(
-        "JIRA_SEVERITY_FIELD", "customfield_11023"
-    ),  # Test environment field
+    "PROJECT_KEY": os.environ.get("JIRA_PROJECT_KEY", "TESTINC"),
+    "SEVERITY_FIELD": os.environ.get("JIRA_SEVERITY_FIELD", "customfield_11023"),
 }
