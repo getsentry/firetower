6385b15e56b662085a0d3e6d2db2c623e3a99040
Spencer Murray <spencer.murray@sentry.io>
2025-10-14 16:26:54 -0400

    Add comment about polyfill

--- frontend/utils/testing-library.ts
+++ frontend/utils/testing-library.ts
@@ -3,7 +3,7 @@ import * as matchers from '@testing-library/jest-dom/matchers';
 import {cleanup} from '@testing-library/react';
 import {afterEach, expect} from 'bun:test';
 
-// Polyfill React.act for React 19 compatibility
+// Polyfill React.act for React 19 compatibility (spencer: this is only an issue for my cursor-agent's shell it seems :shrug:)
 if (!React.act) {
   // @ts-expect-error type not matching on override
   React.act = (callback: () => void | Promise<void>) => {
