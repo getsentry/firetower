8b061e5799420a00ff25d6c1e838f3471319a9bb
Spencer Murray <spencer.murray@sentry.io>
2025-10-21 12:08:58 -0400

    Add required Enums in advance of actual model creation

--- src/firetower/incidents/models.py
+++ src/firetower/incidents/models.py
@@ -1,1 +1,39 @@
-# Create your models here.
+from django.db import models
+
+
+class IncidentStatus(models.TextChoices):
+    ACTIVE = "Active", "Active"
+    MITIGATED = "Mitigated", "Mitigated"
+    POSTMORTEM = "Postmortem", "Postmortem"
+    ACTIONS_PENDING = "Actions Pending", "Actions Pending"
+    DONE = "Done", "Done"
+
+
+class IncidentSeverity(models.TextChoices):
+    P0 = "P0", "P0"
+    P1 = "P1", "P1"
+    P2 = "P2", "P2"
+    P3 = "P3", "P3"
+    P4 = "P4", "P4"
+
+
+class TagType(models.TextChoices):
+    AFFECTED_AREA = "AFFECTED_AREA", "Affected Area"
+    ROOT_CAUSE = "ROOT_CAUSE", "Root Cause"
+
+
+class ExternalLinkType(models.TextChoices):
+    SLACK = "SLACK", "Slack"
+    JIRA = "JIRA", "Jira"
+    DATADOG = "DATADOG", "Datadog"
+    PAGERDUTY = "PAGERDUTY", "PagerDuty"
+    STATUSPAGE = "STATUSPAGE", "StatusPage"
+    NOTION = "NOTION", "Notion"
+    LINEAR = "LINEAR", "Linear"
+
+
+class ExternalProfileType(models.TextChoices):
+    SLACK = "SLACK", "Slack"
+    PAGERDUTY = "PAGERDUTY", "PagerDuty"
+    LINEAR = "LINEAR", "Linear"
+    DATADOG = "DATADOG", "Datadog"
