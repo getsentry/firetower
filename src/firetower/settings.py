72d80ca9d24cb758ca493d543f89a923379d7344
Spencer Murray <spencer.murray@sentry.io>
2025-10-21 12:35:58 -0400

    Add core models and initial migrations

--- src/firetower/settings.py
+++ src/firetower/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
+    "firetower.auth",
     "firetower.incidents",
     "firetower.integrations",
 ]
