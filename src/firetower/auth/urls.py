2cbf043fde529ed72193a1f40173dfe28de31181
Spencer Murray <spencer.murray@sentry.io>
2025-10-31 19:22:30 -0400

    Refactor api URL imports

--- src/firetower/auth/urls.py
+++ src/firetower/auth/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import current_user
 
 urlpatterns = [
-    path("api/ui/users/me/", current_user, name="current-user"),
+    path("ui/users/me/", current_user, name="current-user"),
 ]
--- src/firetower/urls.py
+++ src/firetower/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("firetower.auth.urls")),
-    path("", include("firetower.incidents.urls")),
+    path("api/", include("firetower.auth.urls")),
+    path("api/", include("firetower.incidents.urls")),
 ]
