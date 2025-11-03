cf3610d2b67cf6a8f89b6916495a2f2ddf5adc4f
Spencer Murray <spencer.murray@sentry.io>
2025-11-03 18:44:20 -0500

    Fix vertical alignment of pill

--- frontend/src/components/Pill.tsx
+++ frontend/src/components/Pill.tsx
@@ -4,16 +4,13 @@ import {cn} from 'utils/cn';
 
 const pill = cva(
   [
-    'inline-flex',
-    'items-center',
-    'justify-center',
     'px-space-lg',
     'py-space-xs',
     'rounded-radius-full',
     'text-size-sm',
     'font-medium',
     'uppercase',
-    'leading-compressed',
+    'leading-none ',
   ],
   {
     variants: {
@@ -49,13 +46,19 @@ const pill = cva(
 );
 
 interface PillProps
-  extends React.HTMLAttributes<HTMLSpanElement>,
+  extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof pill> {
-  ref?: React.Ref<HTMLSpanElement>;
+  ref?: React.Ref<HTMLDivElement>;
 }
 
-const Pill = ({className, variant, ref, ...props}: PillProps) => (
-  <span ref={ref} className={cn(pill({variant, className}))} {...props} />
-);
+const Pill = ({className, variant, ref, ...props}: PillProps) => {
+  const {children, ...rest} = props;
+  return (
+    <div ref={ref} className={cn(pill({variant, className}))} {...rest}>
+      {/* mt-[2px] and mb-auto is to align the text inside the div properly. I think the font is messed up slightly based on internet reading.*/}
+      <p className="mt-[2px] mb-auto">{children}</p>
+    </div>
+  );
+};
 
 export {Pill, type PillProps};
