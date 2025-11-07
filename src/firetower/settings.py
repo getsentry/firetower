9549e2f872a565e81aa7cec0b9ed04497a2a262c
Taylor Osler <taylor.osler@sentry.io>
2025-11-06 17:48:26 -0800

    django: adjust static asset path

--- src/firetower/settings.py
+++ src/firetower/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/backend/"
+STATIC_URL = "backend/"
 STATIC_ROOT = "static/backend/"
 
 # Default primary key field type
