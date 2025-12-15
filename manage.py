e20d94830851561b32f7383c43b3d78ca8af2b17
Taylor Osler <taylor.osler@sentry.io>
2025-12-15 12:43:17 -0800

    try again, but this time with signals

--- manage.py
+++ manage.py
@@ -3,35 +3,9 @@
 
 import os
 import sys
-from logging import warning
-
-from datadog.api.events import Event
-from datadog.api.exceptions import DatadogException
 
 
-def log_datadog_event(
-    title: str, text: str = "", alert_type: str = "info", tags: list[str] = []
-) -> None:
-    try:
-        # We may not be able to load settings.py yet -- some commands don't
-        # require it, but it should be set properly in prod.
-        from firetower.settings import env_is_dev
-
-        if not env_is_dev():
-            tags += ["source:firetower"]
-            result = Event.create(
-                True,
-                text=str(repr(sys.argv)),
-                tags=tags,
-                alert_type=alert_type,
-            )
-            print(repr(result))
-    except (ImportError, DatadogException) as e:
-        warning(f"Failed to log datadog event: {e}")
-        print(f"Failed to log datadog event: {e}")
-
-
-def django_main():
+def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firetower.settings")
     try:
@@ -43,35 +17,6 @@ def django_main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-def main():
-    log_datadog_event(
-        title="Firetower: Start Run Django Command",
-        text=str(repr(sys.argv)),
-    )
-
-    exc = None
-    try:
-        django_main()
-    except (Exception, SystemExit) as e:
-        exc = e
-
-    title = "Firetower: Finish Run Django Command"
-    text = str(repr(sys.argv))
-    exit_code = 0
-    if exc is not None:
-        if isinstance(exc, SystemExit):
-            exit_code = exc.code
-            text += f"\n\nExit code: {exit_code}"
-        else:
-            text += f"\n\nException: {exc.__class__}: {exc}"
-    log_datadog_event(
-        title=title,
-        text=text,
-    )
-    if exc is not None:
-        raise exc
 
 
 if __name__ == "__main__":
---
+++ src/firetower/incidents/metrics/migrations.py
@@ -0,0 +1,46 @@
+from logging import warning
+from typing import TypedDict, Unpack
+
+from datadog.api.events import Event
+from datadog.api.exceptions import DatadogException
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate, pre_migrate
+from django.dispatch import receiver
+
+from firetower.settings import env_is_dev
+
+
+class MigrationSignalArgs(TypedDict):
+    plan: tuple[str, bool]
+
+
+def log_datadog_event(
+    title: str, text: str = "", alert_type: str = "info", tags: list[str] = []
+) -> None:
+    if not env_is_dev():
+        try:
+            tags += ["source:firetower"]
+            Event.create(
+                True,
+                text=text,
+                tags=tags,
+                alert_type=alert_type,
+            )
+        except (ImportError, DatadogException) as e:
+            warning(f"Failed to log datadog event: {e}")
+
+
+@receiver(pre_migrate)
+def log_pre_migrate(_sender: AppConfig, **kwargs: Unpack[MigrationSignalArgs]) -> None:
+    plan = kwargs.get("plan")
+    log_datadog_event(
+        title="Firetower: Django migration started", text=f"Plan:\n\n{repr(plan)}"
+    )
+
+
+@receiver(post_migrate)
+def log_post_migrate(_sender: AppConfig, **kwargs: Unpack[MigrationSignalArgs]) -> None:
+    plan = kwargs.get("plan")
+    log_datadog_event(
+        title="Firetower: Django migration finished", text=f"Plan:\n\n{repr(plan)}"
+    )
