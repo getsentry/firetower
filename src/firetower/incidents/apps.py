defac3d0839ddfa54261d7ea06659ff845985623
Taylor Osler <1066487+keeakita@users.noreply.github.com>
2025-12-16 15:43:47 -0800

    Log Migrations to Datadog Event (#72)
    This will make it easier to tell if an issue is caused by a migration
    without digging through Cloud Run logs

--- src/firetower/incidents/apps.py
+++ src/firetower/incidents/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class IncidentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "firetower.incidents"
+
+    def ready(self) -> None:
+        # Load migration logging signals
+        import firetower.incidents.metrics.migrations  # noqa: F401, PLC0415
---
+++ src/firetower/incidents/metrics/migrations.py
@@ -0,0 +1,49 @@
+from logging import debug, warning
+from typing import TypedDict, Unpack
+
+from datadog.api.events import Event
+from datadog.api.exceptions import DatadogException
+from django.db.models.signals import post_migrate, pre_migrate
+from django.dispatch import receiver
+
+from firetower.settings import env_is_dev
+
+
+class MigrationSignalArgs(TypedDict):
+    plan: tuple[str, bool]
+
+
+def log_datadog_event(
+    title: str, text: str = "", alert_type: str = "info", tags: list[str] = []
+) -> None:
+    if not env_is_dev():
+        try:
+            final_tags = tags + ["source:firetower"]
+            result = Event.create(
+                True,
+                title=title,
+                text=text,
+                tags=final_tags,
+                alert_type=alert_type,
+            )
+            debug(f"datadog log: {repr(result)}")
+        except (ImportError, DatadogException) as e:
+            warning(f"Failed to log datadog event: {e}")
+
+
+@receiver(pre_migrate)
+def log_pre_migrate(**kwargs: Unpack[MigrationSignalArgs]) -> None:
+    plan = kwargs.get("plan")
+    if plan is not None and len(plan) > 0:
+        log_datadog_event(
+            title="Firetower: Django migration started", text=f"Plan:\n\n{repr(plan)}"
+        )
+
+
+@receiver(post_migrate)
+def log_post_migrate(**kwargs: Unpack[MigrationSignalArgs]) -> None:
+    plan = kwargs.get("plan")
+    if plan is not None and len(plan) > 0:
+        log_datadog_event(
+            title="Firetower: Django migration finished", text=f"Plan:\n\n{repr(plan)}"
+        )
