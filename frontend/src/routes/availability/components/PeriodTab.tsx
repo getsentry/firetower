cdf566046cbce89ca000e4a05ad947c0c773698f
Khanh Nguyen <khanh.nguyen@sentry.io>
2026-02-23 16:10:13 -0800

    updated rollover colors to be the same as the main page

--- frontend/src/routes/availability/components/PeriodTab.tsx
+++ frontend/src/routes/availability/components/PeriodTab.tsx
@@ -19,8 +19,9 @@ export function PeriodTab({period, label, isActive}: PeriodTabProps) {
       className={cn(
         'rounded-radius-sm px-space-lg py-space-sm text-size-sm font-medium transition-colors',
         {
-          'bg-background-primary text-content-headings shadow-sm': isActive,
-          'text-content-secondary hover:text-black': !isActive,
+          'bg-background-primary dark:bg-background-transparent-neutral-muted text-content-headings shadow-sm':
+            isActive,
+          'text-content-secondary hover:text-black dark:hover:text-white': !isActive,
         }
       )}
     >
