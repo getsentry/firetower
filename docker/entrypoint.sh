cfe0808fb274af186aed1ff1344fe63fccbc1550
Spencer Murray <spencer.murray@sentry.io>
2026-02-24 12:39:16 -0500

    Add run_slack_bot management command and Docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,10 @@ if [ z"$1" = "zmigrate" ]; then
     COMMAND="/app/.venv/bin/django-admin migrate --settings firetower.settings"
 elif [ z"$1" = "zserver" ]; then
     COMMAND="/app/.venv/bin/granian --interface wsgi --host 0.0.0.0 --port $PORT firetower.wsgi:application"
+elif [ z"$1" = "zslack-bot" ]; then
+    COMMAND="/app/.venv/bin/django-admin run_slack_bot --settings firetower.settings"
 else
-    echo "Usage: $0 (migrate|server)"
+    echo "Usage: $0 (migrate|server|slack-bot)"
     exit 1
 fi
 
