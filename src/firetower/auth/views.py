2a512a6e934f1f2f2588cde93ee6c6a146ef1a76
Spencer Murray <spencer.murray@sentry.io>
2026-03-09 13:10:00 -0400

    feat: Add users list endpoint (#122)
    Adds a paginated, searchable user list endpoint at `GET /api/users/`.
    Supports `?search=` for case-insensitive filtering across name and
    email, sorted by email.
    
    Ref:
    https://linear.app/getsentry/issue/RELENG-503/add-users-list-endpoint

--- src/firetower/auth/views.py
+++ src/firetower/auth/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
+from django.db.models import Q, QuerySet, Value
+from django.db.models.functions import Concat
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import UserListSerializer, UserSerializer
 
 
 @api_view(["GET"])
@@ -14,3 +18,19 @@ def current_user(request: Request) -> Response:
     """
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
+
+
+class UserListView(generics.ListAPIView):
+    serializer_class = UserListSerializer
+
+    def get_queryset(self) -> QuerySet[User]:
+        queryset = User.objects.select_related("userprofile").order_by("email")
+        search = self.request.query_params.get("search")
+        if search:
+            queryset = queryset.annotate(
+                full_name=Concat("first_name", Value(" "), "last_name")
+            ).filter(Q(full_name__icontains=search) | Q(email__icontains=search))
+        return queryset
+
+
+user_list = UserListView.as_view()
