e1ceb7d47a7ba97ccbef626a492c16bef5c6db7d
Spencer Murray <spencer.murray@sentry.io>
2026-03-25 13:33:03 -0400

    Handle validation errors and edge cases in command handlers

--- src/firetower/slack_app/handlers/utils.py
+++ src/firetower/slack_app/handlers/utils.py
@@ -4,7 +4,7 @@ from firetower.incidents.models import ExternalLink, ExternalLinkType, Incident
 def get_incident_from_channel(channel_id: str) -> Incident | None:
     link = ExternalLink.objects.filter(
         type=ExternalLinkType.SLACK,
-        url__contains=channel_id,
+        url__endswith=channel_id,
     ).first()
     if link:
         return link.incident
