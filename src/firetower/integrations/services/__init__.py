c49f565ba6835136f25e65a6bcac23eb6c47a020
Spencer Murray <spencer.murray@sentry.io>
2026-03-25 09:47:26 -0400

    Add PagerDuty service client

--- src/firetower/integrations/services/__init__.py
+++ src/firetower/integrations/services/__init__.py
@@ -1,5 +1,6 @@
 """Services package for external integrations."""
 
+from .pagerduty import PagerDutyService
 from .slack import SlackService
 
-__all__ = ["SlackService"]
+__all__ = ["PagerDutyService", "SlackService"]
