8efa528a12815ac5f49d6e1351bf3d2973d20888
Spencer Murray <spencer.murray@sentry.io>
2026-03-25 09:47:45 -0400

    Move integration tests into tests/ subdirectory and add PagerDuty tests

--- src/firetower/integrations/tests/test_slack.py
+++ src/firetower/integrations/tests/test_slack.py
@@ -7,7 +7,7 @@ from unittest.mock import MagicMock, patch
 
 from slack_sdk.errors import SlackApiError
 
-from .services.slack import SlackService
+from firetower.integrations.services.slack import SlackService
 
 # Set up Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firetower.settings")
