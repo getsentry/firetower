b3aa43c710395b4987a5fe8e418137c8c292bfcb
Spencer Murray <spencer.murray@sentry.io>
2026-04-14 16:20:17 -0400

    Reorder help text and simplify stub messages

--- src/firetower/slack_app/handlers/dumpslack.py
+++ src/firetower/slack_app/handlers/dumpslack.py
@@ -3,7 +3,4 @@ from typing import Any
 
 def handle_dumpslack_command(ack: Any, command: dict, respond: Any) -> None:
     ack()
-    respond(
-        "Dumpslack is not yet implemented in Firetower."
-        " Use `/inc dumpslack` in the meantime."
-    )
+    respond("Dumpslack is not yet implemented.")
--- src/firetower/slack_app/handlers/statuspage.py
+++ src/firetower/slack_app/handlers/statuspage.py
@@ -3,7 +3,4 @@ from typing import Any
 
 def handle_statuspage_command(ack: Any, command: dict, respond: Any) -> None:
     ack()
-    respond(
-        "Statuspage is not yet implemented in Firetower."
-        " Use `/inc statuspage` in the meantime."
-    )
+    respond("Statuspage is not yet implemented.")
