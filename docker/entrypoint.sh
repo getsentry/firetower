01d444ff10c39205504e73b31724348edff8b692
Taylor Osler <taylor.osler@sentry.io>
2026-04-30 14:52:47 -0700

    rework entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,16 +4,17 @@ set -e
 set -u
 set -x
 
+export PYTHONPATH="/app:${PYTHONPATH-}"
+
 if [ z"$1" = "zmigrate" ]; then
-    COMMAND="/app/.venv/bin/django-admin migrate --settings firetower.settings"
+    exec /app/.venv/bin/ddtrace-run /app/.venv/bin/django-admin migrate --settings firetower.settings
 elif [ z"$1" = "zserver" ]; then
-    COMMAND="/app/.venv/bin/granian --interface wsgi --host 0.0.0.0 --port $PORT firetower.wsgi:application"
+    exec /app/.venv/bin/ddtrace-run /app/.venv/bin/granian --interface wsgi --host 0.0.0.0 --port "${PORT}" firetower.wsgi:application
 elif [ z"$1" = "zslack-bot" ]; then
-    COMMAND="/app/.venv/bin/django-admin run_slack_bot --settings firetower.settings"
+    exec /app/.venv/bin/ddtrace-run /app/.venv/bin/django-admin run_slack_bot --settings firetower.settings
+elif [ z"$1" = "zworker" ]; then
+    exec /app/.venv/bin/ddtrace-run /app/.venv/bin/django-admin qcluster --settings firetower.settings
 else
-    echo "Usage: $0 (migrate|server|slack-bot)"
+    echo "Usage: $0 (migrate|server|slack-bot|worker)"
     exit 1
 fi
-
-export PYTHONPATH=/app:\$PYTHONPATH
-/app/.venv/bin/ddtrace-run $COMMAND
