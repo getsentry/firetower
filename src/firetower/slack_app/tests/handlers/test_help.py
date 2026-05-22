8ebb71cfbdc980520dc5011fa646da760600d878
Richard Gibert <richard.gibert@sentry.io>
2026-05-22 16:17:34 -0400

    Revert "ref(slack): Use Block Kit for help command output"
    This reverts commit 4d0aafb66eca3965b8793dff08e86c72bd4e29c8.

--- src/firetower/slack_app/tests/handlers/test_help.py
+++ src/firetower/slack_app/tests/handlers/test_help.py
@@ -24,13 +24,9 @@ class TestHandleCommand:
 
         ack.assert_called_once()
         respond.assert_called_once()
-        blocks = respond.call_args[1]["blocks"]
-        block_texts = " ".join(str(b.get("text", "")) for b in blocks)
-        assert "Firetower Slack App" in block_texts
-        field_texts = " ".join(
-            f.get("text", "") for b in blocks for f in b.get("fields", [])
-        )
-        assert "/ft help" in field_texts
+        response_text = respond.call_args[0][0]
+        assert "Firetower Slack App" in response_text
+        assert "/ft help" in response_text
 
     @patch("firetower.slack_app.bolt.statsd")
     def test_empty_text_returns_help(self, mock_statsd, _mock_close):
@@ -43,9 +39,8 @@ class TestHandleCommand:
 
         ack.assert_called_once()
         respond.assert_called_once()
-        blocks = respond.call_args[1]["blocks"]
-        block_texts = " ".join(str(b.get("text", "")) for b in blocks)
-        assert "Firetower Slack App" in block_texts
+        response_text = respond.call_args[0][0]
+        assert "Firetower Slack App" in response_text
 
     @patch("firetower.slack_app.bolt.statsd")
     def test_unknown_subcommand_returns_error(self, mock_statsd, _mock_close):
@@ -72,11 +67,8 @@ class TestHandleCommand:
         handle_command(ack=ack, body=body, command=command, respond=respond)
 
         ack.assert_called_once()
-        blocks = respond.call_args[1]["blocks"]
-        field_texts = " ".join(
-            f.get("text", "") for b in blocks for f in b.get("fields", [])
-        )
-        assert "/ft-test help" in field_texts
+        response_text = respond.call_args[0][0]
+        assert "/ft-test help" in response_text
 
     @patch("firetower.slack_app.bolt.statsd")
     def test_emits_submitted_and_completed_metrics(self, mock_statsd, _mock_close):
