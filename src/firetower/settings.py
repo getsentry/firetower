737d6d2bc74459e44fce48669af10be2c86759b7
Spencer Murray <spencer.murray@sentry.io>
2026-09-23 10:46:43 -0400

    Allow private Cloud Run API hostnames

--- src/firetower/settings.py
+++ src/firetower/settings.py
@@ -106,6 +106,7 @@ ALLOWED_HOSTS = [
     "::1",
     "firetower.getsentry.net",
     "test.firetower.getsentry.net",
+    ".run.app",  # generated Cloud Run host used for private service-to-service calls
     "firetower",
     "backend",  # name from the nginx proxy_pass
 ]
